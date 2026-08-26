19f6549f1642d320291c97f90e74c80ba3a0244f
Daniel Granato <dgmgranato@gmail.com>
2026-08-26 10:40:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg" alt="logo" />
+      </header>
+      <section>
+        <image src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg" alt="logo" />
+      </section>
+    </main>
+  )
+}
+
+export default App
