733e87cb8abb78e2745e5d96e52d1ff7c75730f0
Daniel Granato <dgmgranato@gmail.com>
2026-08-28 13:41:35 -0300

    add form, css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,55 @@
 import './App.css'
 
+function FormularioDeEvento() {
+  function handleSubmit(event) {
+    event.preventDefault()
+  }
+
+  return (
+    <form className="event-form" onSubmit={handleSubmit}>
+      <h1>Preencha para criar um evento:</h1>
+
+      <div className="form-fields">
+        <div className="form-field">
+          <label htmlFor="event-name">Qual o nome do evento?</label>
+          <input
+            id="event-name"
+            name="eventName"
+            type="text"
+            placeholder="Summer dev hits"
+            required
+          />
+        </div>
+
+        <div className="form-field">
+          <label htmlFor="event-date">Data do evento</label>
+          <input
+            id="event-date"
+            name="eventDate"
+            type="text"
+            placeholder="XX/XX/XXXX"
+            required
+          />
+        </div>
+
+        <div className="form-field">
+          <label htmlFor="event-theme">Tema do evento</label>
+          <select id="event-theme" name="eventTheme" defaultValue="" required>
+            <option value="" disabled>
+              Selecione uma opção
+            </option>
+            <option value="frontend">Front-end</option>
+            <option value="backend">Back-end</option>
+            <option value="fullstack">Full Stack</option>
+          </select>
+        </div>
+      </div>
+
+      <button type="submit">Criar evento</button>
+    </form>
+  )
+}
+
 function App() {
   return (
     <main>
@@ -8,6 +58,7 @@ function App() {
       </header>
       <section>
         <img src="/banner.png" alt="Banner Tecboard" />
+        <FormularioDeEvento />
       </section>
     </main>
   )
