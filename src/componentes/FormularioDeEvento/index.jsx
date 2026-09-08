01a051ee3472a7374e779c66c2879a95c9791f4b
Daniel Granato <dgmgranato@gmail.com>
2026-09-08 17:57:23 -0400

    feat: handle event form with FormData

--- src/componentes/FormularioDeEvento/index.jsx
+++ src/componentes/FormularioDeEvento/index.jsx
@@ -8,13 +8,30 @@ import ListaSuspensa from '../ListaSuspensa';
 
 function FormularioDeEvento( {temas} ) {
 
+  function aoFormSubmetido(formData) {
+    console.log('opa, tá na hora de criar um novo evento', formData)
+    const evento = {
+      capa: formData.get('capa'),
+      tema: temas.find(function (item) {
+        return item.id == formData.get('tema')
+      }),
+      data: new Date(formData.get('dataEvento')),
+      titulo: formData.get('nomeEvento')
+    }
+    aoSubmeter(evento)
+  }
+
   return (
-    <form className="form-evento">
+    <form className="form-evento" action={aoFormSubmetido}>
       <TituloFormulario>Preencha para criar um evento:</TituloFormulario>
       <div className="campos">
       <CampoDeFormulario>
         <Label htmlFor="nome">Qual o nome do evento?</Label>
         <CampoDeEntrada type="text" id="nome" name="nome" />
+      </CampoDeFormulario>
+      <CampoDeFormulario>
+        <Label htmlFor="capa">Qual o endereço da imagem de capa?</Label>
+        <CampoDeEntrada type="text" id="capa" name="capa" />
       </CampoDeFormulario>
       <CampoDeFormulario>
         <Label htmlFor="data">Data do evento</Label>
--- src/componentes/ListaSuspensa/index.jsx
+++ src/componentes/ListaSuspensa/index.jsx
@@ -1,8 +1,8 @@
 import './lista-suspensa.css';
 
-function ListaSuspensa ( {itens} ) {
+function ListaSuspensa ( {itens, ...rest} ) {
     return (
-        <select className="lista-suspensa-form" defaultValue="">
+        <select {...rest} className="lista-suspensa-form" defaultValue="">
             <option value="" disabled>Selecione uma opção</option>
             {itens.map(function(item) {
                 return <option key={item.id} value={item.id}>{item.nome}</option>
